61a089ed55285e03bf88053660077e9347bba8c1
tomassaana <satskovt@gmail.com>
2026-05-05 16:55:14 +0300

    mod  all files

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,41 @@
-#include  <iostream>
+#include <GLFW/glfw3.h>
 
+int main(void)
+{
+    GLFWwindow* window;
 
-int main(int argc,char* argv[] ){
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
-         std::cout <<"Hello!" << std::endl;
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
 
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
 
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        // openGL function to clear the color buffer, 
+        // which is the buffer that holds the color values for each pixel on the screen. 
+        // This is typically done at the beginning of each frame to clear the previous frame's 
+        // contents and prepare for drawing the new frame.
+		//glClear(GL_COLOR_BUFFER_BIT); ----------------------------
+        
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
 
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
 
-return 0;}
+    glfwTerminate();
+    return 0;
+}
