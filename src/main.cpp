1c3c7eafbd3de842a5d411f94fd1e23b401e7d24
tomassaana <satskovt@gmail.com>
2026-05-05 17:32:43 +0300

    opengl added!

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include<glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -19,6 +21,16 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+	// gladLoadGLLoader is a function provided by the GLAD library, which is used to load OpenGL function pointers at runtime.
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to load GLAD" << std::endl;
+        return -1;
+	}
+	std::cout << "OpenGL version: " << GLVersion.major << "." << GLVersion.minor << std::endl;
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Set the clear color to a specific RGBA value (in this case, a shade of teal)
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -27,8 +39,8 @@ int main(void)
         // which is the buffer that holds the color values for each pixel on the screen. 
         // This is typically done at the beginning of each frame to clear the previous frame's 
         // contents and prepare for drawing the new frame.
-		//glClear(GL_COLOR_BUFFER_BIT); ----------------------------
-        
+		glClear(GL_COLOR_BUFFER_BIT);
+         
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
